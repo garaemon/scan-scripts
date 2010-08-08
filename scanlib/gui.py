6492235174cb3edf49b3510ff61a8d58209eda22
garaemon <garaemon@gmail.com>
2010-08-09 08:10:09 +0900

    update

--- scanlib/gui.py
+++ scanlib/gui.py
@@ -1,6 +1,7 @@
 from wx import *
 import os
 from image import *
+import copy
 
 class ScanController(wx.Frame):
     dirname = ''
@@ -8,11 +9,10 @@ class ScanController(wx.Frame):
     _files = []
     def __init__(self, parent, title):
         wx.Frame.__init__(self, parent, title=title, size=(200,100))
-        
-        
         hsv_bar = self.make_hsv_sliders("parameters for color extraction")
         # buttons
         self.run_button = wx.Button(self, -1, "run")
+        self.color_extract_button = wx.Button(self, -1, "Color Extract")
         self.quit_button = wx.Button(self, -1, "Quit")
         
         # menu bar
@@ -32,11 +32,13 @@ class ScanController(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
         self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
         self.Bind(wx.EVT_BUTTON, self.OnExit, self.quit_button)
+        self.Bind(wx.EVT_BUTTON, self.ColorExtract, self.color_extract_button)
         
         #sizer
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         #self.sizer.Add(self.slider1, 0, wx.EXPAND)
         self.sizer.Add(hsv_bar, 0, wx.EXPAND)
+        self.sizer.Add(self.color_extract_button, 1, wx.EXPAND)
         self.sizer.Add(self.run_button, 1, wx.EXPAND)
         self.sizer.Add(self.quit_button, 0, wx.EXPAND)
         self.SetSizer(self.sizer)
@@ -48,8 +50,8 @@ class ScanController(wx.Frame):
         sizer = wx.BoxSizer(wx.VERTICAL)
         slider_text = wx.StaticText(self, -1, text)
         slider_flag = wx.SL_HORIZONTAL | wx.SL_AUTOTICKS | wx.SL_LABELS
-        slider_h_max = self.make_a_bar("h_max", slider_flag)
-        slider_h_min = self.make_a_bar("h_min", slider_flag)
+        slider_h_max = self.make_a_bar("h_max", slider_flag, 360)
+        slider_h_min = self.make_a_bar("h_min", slider_flag, 360)
         slider_s_max = self.make_a_bar("s_max", slider_flag)
         slider_s_min = self.make_a_bar("s_min", slider_flag)
         slider_v_max = self.make_a_bar("v_max", slider_flag)
@@ -63,20 +65,32 @@ class ScanController(wx.Frame):
         sizer.Add(slider_v_min, 0, wx.EXPAND)
         return sizer
     # callbacks
-    def make_a_bar(self, atext, slider_flag):
+    def make_a_bar(self, atext, slider_flag, _max = 255.0):
         sizer = wx.BoxSizer(wx.HORIZONTAL)
         text = wx.StaticText(self, -1, atext)
-        slider = wx.Slider(self, -1, 50, 0, 255,
+        initval = _max / 2
+        self._slider_var[atext] = initval
+        slider = wx.Slider(self, -1, initval, 0, _max,
                            size = (300, -1),
                            style = slider_flag)
         sizer.Add(text, 0, wx.EXPAND)
+        
         sizer.Add(slider, 0, wx.EXPAND)
         def local_func(evt):
             self._slider_var[atext] = slider.GetValue()
-            print self._slider_var
+            return
         slider.Bind(wx.EVT_SLIDER, local_func)
         return sizer
-    def OnAbout(self,e):
+    def ColorExtract(self, e):
+        # h -> 0 ~ 360 (no scaling)
+        # s, v -> 0 ~ 1.0 (scale by 255)
+        scaled_param = copy.deepcopy(self._slider_var)
+        for v in ['s_min', 's_max', 'v_min', 'v_max']:
+            scaled_param[v] /= 255.0
+        i = ImageContainer(self._files[0])
+        i.binalize(*scaled_param)
+        print scaled_param
+    def OnAbout(self, e):
         # Create a message dialog box
         dlg = wx.MessageDialog(self,
                                """
--- scanlib/image.py
+++ scanlib/image.py
@@ -29,6 +29,7 @@ class PILImageFrame(wx.Frame):
 class ImageContainer(object):
     _path = False
     _org_image = False                  # PIL Image
+    _binary_image = False
     _showedp = False
     def __init__(self, path):
         self._path = path
@@ -42,6 +43,11 @@ class ImageContainer(object):
         f.Show()
     def showedp(self):
         return self._showedp
+    def binalize(self, h_min, h_max, s_min, s_max, v_min, v_max):
+        self._binary_image = extract_color(self._org_image, h_min, h_max,
+                                           s_min, s_max, v_min, v_max)
+        f = PILImageFrame(self._binary_image)
+        f.Show()
     
 def load_image(file):
     """
@@ -50,15 +56,16 @@ def load_image(file):
     return Image.open(os.path.abspath(file))
 
 def extract_color(image,
-                  h_min = 0, h_max = 255,
-                  s_min = 0, s_max = 255,
-                  v_min = 0, v_max = 255):
+                  h_min = 0, h_max = 360,
+                  s_min = 0, s_max = 1.0,
+                  v_min = 0, v_max = 1.0):
     ret = image.copy()
     pix = image.load()
     ret_pix = ret.load()
     for x in range(image.size[0]):
         for y in range(image.size[1]):
             pixel = pix[x, y]
+            hsv_pixel = RGB2HSV(pix[0], pix[1], pix[2])
             h = pixel[0]
             s = pixel[1]
             v = pixel[2]
@@ -72,3 +79,68 @@ def edge_detection(image):
     # canny
     # hough
     return True
+
+# RGB -> HSV
+# R, G, B, S, V -> 0 ~ 1
+# h -> 0 ~ 360
+def RGB2HSV( r, g, b ):
+    r, g, b = map( float, (r,g,b ) )
+    if r == g and g == b:
+        ma = r
+        mi = r
+        h = 0
+        v = r
+    elif r > g and r > b:
+        ma = r
+        mi = min( g , b )
+        h = 60 * ( g - b ) / ( ma - mi ) + 0
+    elif g > b and g >= r:
+        ma = g
+        mi = min( b , r )
+        h = 60 * ( b - r ) / ( ma - mi ) + 120
+    elif b >= r and b >= g:
+        ma = b
+        mi = min( r , g )
+        h = 60 * ( r - g ) / ( ma - mi ) + 240
+    else:
+        raise Exception("error")
+    if ma == 0:
+        return 0, 0, 0
+    s = (ma - mi) / ma
+    v = ma
+    return h, s, v
+
+# HSV -> RGB
+def HSV2RGB( h, s, v ):
+    hi = int(h / 60) % 6
+    f = h / 60 - hi
+    p = v * ( 1 - s )
+    q = v * ( 1 - f * s )
+    t = v * ( 1 - ( 1 - f ) * s )
+
+    if hi == 0:
+        r = int(round( v ))
+        g = int(round( t ))
+        b = int(round( p ))
+    elif hi == 1:
+        r = int(round( q ))
+        g = int(round( v ))
+        b = int(round( p ))
+    elif hi == 2:
+        r = int(round( p ))
+        g = int(round( v ))
+        b = int(round( t ))
+    elif hi == 3:
+        r = int(round( p ))
+        g = int(round( q ))
+        b = int(round( v ))
+    elif hi == 4:
+        r = int(round( t ))
+        g = int(round( p ))
+        b = int(round( v ))
+    elif hi == 5:
+        r = int(round( v ))
+        g = int(round( p ))
+        b = int(round( q ))
+
+    return r, g, b
